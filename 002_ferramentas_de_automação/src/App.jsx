c3743ad703c00a620a100ede64975b9a7aaf8b5c
lucasb13 <lucaseduardo131922@gmail.com>
2025-06-18 11:37:24 -0300

    Initial commit

---
+++ 002_ferramentas_de_automação/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <a href="https://www.instagram.com/l.brito13/" target="_Blank">Lucas Brito</a>
+}
+
+export default App;
---
+++ 003_ferramentas_de_frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = ()=> {
+  return (
+    <div>
+       <h1>React Completo</h1>
+       <p>Sou Lucas Brito e estou estudando o curso da <a href="https://www.origamid.com">Origamid</a>.</p>
+     </div>
+  );
+};
+
+export default App
