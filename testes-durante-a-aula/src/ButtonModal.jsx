47050220ba0534ac02d9ff85d16f7f6de7a38fcb
lucasb13 <lucaseduardo131922@gmail.com>
2025-07-04 20:21:00 -0300

    Estudo de Callback no  useState

--- testes-durante-a-aula/src/ButtonModal.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ButtonModal = ({modal, setModal}) => {
-  if (modal) return null;
-
-  return <button onClick={() => setModal(true)}>Abrir modal</button>;
-}
-
-export default ButtonModal;
--- testes-durante-a-aula/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div><a href="#" title='isso é um link'>Footer</a></div>
-  )
-}
-
-export default Footer;
