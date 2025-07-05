0c1dae47173084eba50d40b9c25ab94adf13a1f9
lucasb13 <lucaseduardo131922@gmail.com>
2025-07-04 23:29:34 -0300

    chore: adiciona preview do projeto ao readme

--- 009-desafio-fetch-API-useState/src/App.jsx
+++ 009-desafio-fetch-API-useState/src/App.jsx
@@ -4,30 +4,44 @@ import Produto from './produto';
 const App = () => {
   const [dados, setDados] = React.useState(null);
   const [carregando, setCarregando] = React.useState(null);
+  const [clickedButton, setClickedButton] = React.useState('');
 
   async function handleClick(event) {
-    setCarregando(true)
+    setCarregando(true);
+    const produto = event.target.innerText.toLowerCase();
+    setClickedButton(produto);
     const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+      `https://ranekapi.origamid.dev/json/api/produto/${produto}`,
     );
     const json = await response.json();
     setDados(json);
     setCarregando(false);
   }
 
+  const botaoStyle = (nome) => ({
+    height: '2rem',
+    margin: '.5rem',
+    backgroundColor: clickedButton === nome ? '#555' : '#ccc',
+    color: '#000',
+    border: 'none',
+    padding: '0 1rem',
+    cursor: 'pointer',
+  });
+
   return (
     <div>
-      <button style={{ height: '2rem', margin: '.5rem' }} onClick={handleClick}>
+      <button onClick={handleClick} style={botaoStyle('tablet')}>
         tablet
       </button>
-      <button style={{ height: '2rem', margin: '.5rem' }} onClick={handleClick}>
+      <button onClick={handleClick} style={botaoStyle('notebook')}>
         notebook
       </button>
-      <button style={{ height: '2rem', margin: '.5rem' }} onClick={handleClick}>
+      <button onClick={handleClick} style={botaoStyle('smartphone')}>
         smartphone
       </button>
+
       {carregando && <p>Carregando...</p>}
-      {!carregando && dados && <Produto dados={dados}/>}
+      {!carregando && dados && <Produto dados={dados} />}
     </div>
   );
 };
